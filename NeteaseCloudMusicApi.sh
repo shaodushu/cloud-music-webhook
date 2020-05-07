61515b9da9c9f6b8cda140f9423ac4f2891f247d
少读书 <17615019466@163.com>
2020-05-07 10:24:47 +0800

    fix:docker docker 镜像名称不能为大写

--- NeteaseCloudMusicApi.sh
+++ NeteaseCloudMusicApi.sh
@@ -7,10 +7,10 @@ git reset --hard origin/master
 git clean -f
 echo "拉取新代码"
 git pull origin master
-echo "开始执行构建后端项目:NeteaseCloudMusicApi为docker镜像名称 3.27.0为版本号"
-docker build -t NeteaseCloudMusicApi:3.27.0 .
+echo "开始执行构建后端项目:netease-cloud-music-api为docker镜像名称 3.27.0为版本号"
+docker build -t netease-cloud-music-api:3.27.0 .
 echo "停止旧容器 并删除旧容器"
-docker stop NeteaseCloudMusicApi-container
-docker rm NeteaseCloudMusicApi-container
+docker stop netease-cloud-music-api-container
+docker rm netease-cloud-music-api-container
 echo "启动新容器"
-docker container run -p 3000:3000 --name NeteaseCloudMusicApi-container -d NeteaseCloudMusicApi:3.27.0
+docker container run -p 3000:3000 --name netease-cloud-music-api-container -d netease-cloud-music-api:3.27.0
