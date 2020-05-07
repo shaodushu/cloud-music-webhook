3f46b4ebb10a6bfeae054f247d923fde7ac2e278
少读书 <17615019466@163.com>
2020-05-07 17:13:42 +0800

    feat:cloud-music.sh 更新前端执行脚本

--- cloud-music.sh
+++ cloud-music.sh
@@ -7,11 +7,11 @@ git clean -f
 echo "拉取新代码"
 git pull origin master
 echo "编译build"
-npm run build
-echo "开始执行构建前端项目:cloud-music为docker镜像名称 1.0为版本号"
+yarn run build
+echo "开始执行构建前端项目:cloud-music为docker镜像名称 0.1.0为版本号"
 docker build -t cloud-music:1.0 .
 echo "停止旧容器 并删除旧容器"
-docker stop font-container
-docker rm font-container
+docker stop cloud-music-container
+docker rm cloud-music-container
 echo "启动新容器"
-docker container run -p 80:80 --name font-container -d font:1.0
+docker container run -p 80:80 --name cloud-music-container -d cloud-music:0.1.0
