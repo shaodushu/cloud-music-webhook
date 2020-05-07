966ef928efa97d33dafda81a3adcf1429b4d79f7
少读书 <17615019466@163.com>
2020-05-07 09:16:26 +0800

    feat:init 构建检测更新模块

---
+++ cloud-music.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+WORK_PATH='/data/projects/cloud-music'
+cd $WORK_PATH
+echo "先清除老代码"
+git reset --hard origin/master
+git clean -f
+echo "拉取新代码"
+git pull origin master
+echo "编译build"
+npm run build
+echo "开始执行构建前端项目:cloud-music为docker镜像名称 1.0为版本号"
+docker build -t cloud-music:1.0 .
+echo "停止旧容器 并删除旧容器"
+docker stop font-container
+docker rm font-container
+echo "启动新容器"
+docker container run -p 80:80 --name font-container -d font:1.0
